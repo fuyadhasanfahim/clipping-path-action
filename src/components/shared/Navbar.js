e4e1aaecccafa01c800d2307ac38b2ed0f06e060
Fuyad Hasan Fahim <fuyad.dev@gmail.com>
2024-03-16 16:18:55 +0600

    Navbar

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -239,13 +239,13 @@ export default function Navbar() {
                             <div className="py-6 space-x-4">
                                 <a
                                     href="/login"
-                                    className="rounded-full bg-gray-900 px-3.5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
+                                    className="rounded-full bg-gray-900 px-3.5 py-2 text-lg font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
                                 >
                                     Login <span aria-hidden="true">&rarr;</span>
                                 </a>
                                 <a
                                     href="/sign-up"
-                                    className="rounded-full bg-gray-900 px-3.5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
+                                    className="rounded-full bg-gray-900 px-3.5 py-2 text-lg font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
                                 >
                                     Sign Up{" "}
                                     <span aria-hidden="true">&rarr;</span>
